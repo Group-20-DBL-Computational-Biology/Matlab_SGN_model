99e3a88b042bb112e5a742aa31f503dbfd1b1aa0
JoanKant <j.b.y.kant@student.tue.nl>
2019-11-28 17:33:07 +0100

    some small changes

--- SystemState.m
+++ SystemState.m
@@ -1,13 +1,14 @@
  function dydt = SystemState(t,y, para)
        
-        
+        %ODEs needed for the expression of one protein 
         dydt = [
          0;
         ((para(1) * y(5)) * y(1))/(para(7) + y(1)) - (para(4) * y(2));
         (para(2) * y(6) * y(2))/(para(8) + y(2))- (para(6) * y(3));
         para(6) * y(3);
-        -(para(3) * y(5) * y(1))/(para(7) + y(1));
-        -(para(5) * y(6)) /(para(9) + y(6))] ;        
+        -(para(3) * y(5) * y(1))/(para(7) + y(1))  ;
+        -(para(5) * y(6)) /(para(9) + y(6))] ; %-para(5)*y(6)/(para(9)+y(6))
+    
         
        
 end 
