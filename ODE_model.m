51a8d61d1a4c2721ce48aee69cc93f20ddfeb553
JoanKant <j.b.y.kant@student.tue.nl>
2019-11-28 17:32:49 +0100

    some small changes

--- ODE_model.m
+++ ODE_model.m
@@ -3,12 +3,11 @@ close all
 
 %% input by user
 %define timespan by user
-tspan = [0:0.1:480];
+tspan = 0:0.1:480;
 
 %initial conditions
 DNA0 = 3.4; %can be changed
 
-for i = 1:1:3
 %store the parameter values
 para = [18.2 16.1 1.1e-2 7.8e-4 4.5e-3 0.2 8.5 65.8 6e-5];
     
@@ -17,14 +16,17 @@ initCond = [DNA0 0  0 0 1 1];
 
 %options for ODE solver
 opts = odeset('RElTol', 1e-9, 'NonNegative', 1:6); 
-%solve the ODEs
-ODEFUN = @(t,y) SystemState(t,y,para);
+%Explanation: NonNegative, because all six parameters cannot have a
+%negative concentration. 
+%% Solving the ODE system
+ODEFUN = @(t,y) SystemState(t,y,para); %define your ODE function
 
-[t_values, sol_values] = ode15s(ODEFUN, tspan, initCond, opts); %DNA dmRNAdt, dPdt, dPmatdt, dTsRdt, dTlRdt
+[t_values, sol_values] = ode15s(ODEFUN, tspan, initCond, opts); 
+%output will be in the following order: DNA dmRNAdt, dPdt, dPmatdt, dTsRdt,
+%dTlRdt (DNA will be constant)
 
 %convert minutes to hours
 thours = t_values/60;
-
 
 %% Plotting
 %plot time vs  protein concentration and matured protein concentration
@@ -55,9 +57,3 @@ plot(thours, sol_values(:,3));
 title('Concentration of protein in time');
 xlabel('time (hours)');
 ylabel('concentration (nM)'); 
-end 
-
-
-
-
-
