43395a372312830795411a1b4bd86d3d4d499e38
JoanKant <j.b.y.kant@student.tue.nl>
2019-12-09 18:29:04 +0100

    Interactions repressor allolactose operator and production of galatosidase and repressor

--- ODE_model.m
+++ ODE_model.m
@@ -1,5 +1,5 @@
-clear all;
 close all
+clear all;
 
 %% input by user
 %define timespan by user
@@ -7,53 +7,63 @@ tspan = 0:0.1:480;
 
 %initial conditions
 DNA0 = 3.4; %can be changed
-
+L0 = 100e3; 
 %store the parameter values
-para = [18.2 16.1 1.1e-2 7.8e-4 4.5e-3 0.2 8.5 65.8 6e-5];
+para = [18.2 16.1 1.1e-2 7.8e-4 4.5e-3 8.5 65.8 6e-5 8.33e-4 8.33e-4 60e13 0.8*60 60*7e18 60*6.2e-4 30e6*60 500*60 0.1*60];
     
 %store the initial conditions
-initCond = [DNA0 0  0 0 1 1];
+initCond = [DNA0 0 0 1 10 0 0 DNA0 0 L0 0 0 0];
 
 %options for ODE solver
-opts = odeset('RElTol', 1e-9, 'NonNegative', 1:6); 
+opts = odeset('RElTol', 1e-9, 'NonNegative', 1:13); 
 %Explanation: NonNegative, because all six parameters cannot have a
 %negative concentration. 
 %% Solving the ODE system
 ODEFUN = @(t,y) SystemState(t,y,para); %define your ODE function
 
 [t_values, sol_values] = ode15s(ODEFUN, tspan, initCond, opts); 
-%output will be in the following order: DNA dmRNAdt, dPdt, dPmatdt, dTsRdt,
-%dTlRdt (DNA will be constant)
+%output will be in the following order: [dDNA; dmRNA_lacI; dmRNA_lacZ; dTsR; dTlR; dRA; dR; dO; dRO; dL; dG; dGL; dA];
+
 
 %convert minutes to hours
 thours = t_values/60;
-
 %% Plotting
-%plot time vs  protein concentration and matured protein concentration
+%plot time vs  protein concentration
 figure(1)
-plot(thours, sol_values(:,3), thours,sol_values(:,4));
+plot(thours, sol_values(:,7), thours,sol_values(:,11));
 title('Concentration of protein in time');
 xlabel('time (hours)');
 ylabel('concentration (nM)'); 
-legend('protein', 'matured protein')
+legend('repressor', 'galactosidase')
 
 %plot time vs mrna
 figure(2)
-plot(thours, sol_values(:,2));
+plot(thours, sol_values(:,2),thours, sol_values(:,3) );
 title('Concentration of mRNA in time');
 xlabel('time (hours)');
-ylabel('concentration (nM)'); 
+ylabel('concentration (nM)');
+legend('mRNA lacI', 'mRNA lacZ')
 
 %plot time vs resources
 figure(3)
-plot(thours, sol_values(:,5), thours, sol_values(:,6));
+plot(thours, sol_values(:,4), thours, sol_values(:,5));
 title('Concentration of the resources in time');
 xlabel('time (hours)');
 ylabel('concentration (nM)'); 
+legend('TsR', 'TlR')
 
-%plot time vs GFP (NOT matured)
 figure(4)
-plot(thours, sol_values(:,3));
-title('Concentration of protein in time');
+plot(thours, sol_values(:,10), thours, sol_values(:,13));
+title('Concentration of the resources in time');
 xlabel('time (hours)');
 ylabel('concentration (nM)'); 
+legend('Lactose', 'Allolactose')
+%%
+figure(5)
+plot(thours, sol_values(:,7), thours, sol_values(:,6));
+title('Concentration of the resources in time');
+xlabel('time (hours)');
+ylabel('concentration (nM)'); 
+legend('Repressor', 'complex RA')
+
+
--- SystemState.m
+++ SystemState.m
@@ -1,14 +1,80 @@
  function dydt = SystemState(t,y, para)
-       
-        %ODEs needed for the expression of one protein 
-        dydt = [
-         0;
-        ((para(1) * y(5)) * y(1))/(para(7) + y(1)) - (para(4) * y(2));
-        (para(2) * y(6) * y(2))/(para(8) + y(2))- (para(6) * y(3));
-        para(6) * y(3);
-        -(para(3) * y(5) * y(1))/(para(7) + y(1))  ;
-        -(para(5) * y(6)) /(para(9) + y(6))] ; %-para(5)*y(6)/(para(9)+y(6))
-    
+        %% parameters
+        %link each concentration to the right parameter
+        DNA = y(1);
+        mRNA_lacI = y(2);
+        mRNA_lacZ = y(3);
+        TsR = y(4);
+        TlR = y(5);
+        RA = y(6);
+        R = y(7); 
+        O = y(8);
+        RO = y(9);
+        L = y(10);
+        G = y(11); 
+        GL = y(12); 
+        A = y(13);
         
-       
+        %link each parameter value 
+        kts = para(1); %nanomolar per minute
+        ktl = para(2); %nanomolar per minute
+        kcs = para(3); % per minute
+        deltamRNA = para(4); %per minute 
+        deltaTlr = para(5); %per minute
+        Ks = para(6);% nanomolar
+        Kl = para(7); %nanomolar
+        Ktlr = para(8); %
+        
+        %degradation values
+        deltaR = para(9); %not known yet
+        deltaG = para(10); 
+        
+        %k-values for repressor and allolactose
+        k_on1 = para(11);
+        k_off1 = para(12);
+        
+        %k-values repressor and operator
+        k_on2 = para(13);
+        k_off2 = para(14);
+        
+        %k-values galactosidase and lactose
+        k_on3 = para(15); 
+        k_off3 = para(16); 
+        kcat3 = para(17); 
+        %% ODEs
+        dDNA = 0; 
+        %ODEs needed for the expression lacI
+        %transcription
+        dmRNA_lacI = (kts * TsR * DNA)/(Ks + DNA) - (deltamRNA * mRNA_lacI);
+        %translation + degradation
+        trans_R = (ktl * TlR * mRNA_lacI)/(Kl + mRNA_lacI) -deltaR* R;
+              
+        %ODEs needed for the expression lacZ
+        %transcription
+        dmRNA_lacZ = (kts * TsR * O)/(Ks + O) - (deltamRNA * mRNA_lacZ);
+        %translation  + degradation
+        trans_G = (ktl * TlR * mRNA_lacZ)/(Kl + mRNA_lacZ)- deltaG * G;
+        
+        %Resources for both expressions
+        dTsR = -(kcs * TsR * DNA)/(Ks + DNA);
+        dTlR = -(deltaTlr * TlR) /(Ktlr + TlR);
+        
+        %1) ODEs neede for itneraction between repressor and allolactose 
+        rep_no_allo = -k_on1 * R * A + k_off1 * RA; 
+        free_allo = -k_on1 * R * A + k_off1 * RA; 
+        dRA = k_on1 * R * A -k_off1 * RA; 
+        
+        %2) ODEs needed for interaction between repressor and operator
+        dR = (-k_on2 * R * O + k_off2 * RO) + trans_R + rep_no_allo; %free repressor
+        dO = (-k_on2 * R * O + k_off2 * RO); %free DNA = free operator
+        dRO = (k_on2 * R * O + k_off2 *RO); %complex
+        
+        %ODEs needed for interactions between galactosidase and lactose
+        dL = -k_on3*G * L + k_off3* GL;
+        dG = trans_G + (-k_on3 * G * L + (k_off3 + kcat3) * GL);
+        dGL = k_on3*G * L - (k_off3 + kcat3) * GL; 
+        dA = 0.5*kcat3 * GL + free_allo; 
+
+        %store all solutions in the following array 
+        dydt = [dDNA; dmRNA_lacI; dmRNA_lacZ; dTsR; dTlR; dRA; dR; dO; dRO; dL; dG; dGL; dA];
 end 
